a36d2759589e2bd3c9e87af84cbb782422a47018
Tuomo Kohtamäki <tuomo.kohtamaki@gmail.com>
2018-02-13 16:43:16 +0200

    Uniform soft tabs in all files

--- plot_saved_data.py
+++ plot_saved_data.py
@@ -4,10 +4,10 @@ import glob
 import sys
 
 if len(sys.argv) == 1:
-	list_of_files = glob.glob('traces/*.npz') # * means all if need specific format then *.csv
-	filename = max(list_of_files)
+    list_of_files = glob.glob('traces/*.npz') # * means all if need specific format then *.csv
+    filename = max(list_of_files)
 else:
-	filename = sys.argv[1]
+    filename = sys.argv[1]
 
 saved_data = np.load(filename)
 channel_data = saved_data['channel_data']
@@ -15,10 +15,10 @@ t_data = saved_data['t_data']
 
 # plot graphs
 for count, y_data in enumerate(channel_data):
-	plt.subplot(2, 2, count + 1)
-	plt.plot(t_data, y_data)
-	plt.xlabel('Voltage (V)')
-	plt.ylabel('Time (s)')
-	plt.grid(True)
+    plt.subplot(2, 2, count + 1)
+    plt.plot(t_data, y_data)
+    plt.xlabel('Voltage (V)')
+    plt.ylabel('Time (s)')
+    plt.grid(True)
 
 plt.show()
--- trace_capture.py
+++ trace_capture.py
@@ -13,9 +13,9 @@ channels_enabled = scope.channels_enabled()
 channel_data = list([])
 
 for ch_idx, enabled in enumerate(channels_enabled):
-	if enabled:
-		this_ch_data, t_data = scope.read_trace(ch_idx+1)
-		channel_data.append(np.array(this_ch_data))
+    if enabled:
+        this_ch_data, t_data = scope.read_trace(ch_idx+1)
+        channel_data.append(np.array(this_ch_data))
 
 channel_data = np.array(channel_data)
 
@@ -25,11 +25,11 @@ np.savez_compressed(filename,channel_data=channel_data,t_data=t_data)
 '''
 # plot graphs
 for count, y_data in enumerate(channel_data):
-	plt.subplot(2, 2, count + 1)
-	plt.plot(t_data, y_data)
-	plt.xlabel('Voltage (V)')
-	plt.ylabel('Time (s)')
-	plt.grid(True)
+    plt.subplot(2, 2, count + 1)
+    plt.plot(t_data, y_data)
+    plt.xlabel('Voltage (V)')
+    plt.ylabel('Time (s)')
+    plt.grid(True)
 
 plt.show()
 '''
